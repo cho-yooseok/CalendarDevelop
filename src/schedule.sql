1fd05bfc7466b8b9c0f32981df337c5aadfad904
choyooseok <bluesbeat93@gmail.com>
2025-04-02 07:07:29 +0900

    feat: Add schedule CRUD functionality   추가 일정 CRUD기능

--- src/schedule.sql
+++ src/schedule.sql
@@ -1,12 +1,12 @@
 CREATE TABLE schedule (
-                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          task VARCHAR(255) NOT NULL,
-                          password VARCHAR(255) NOT NULL,
-                          member_name VARCHAR(255) NOT NULL,
-                          created_at DATETIME NOT NULL,
-                          updated_at DATETIME NOT NULL
-)
-
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    member_id BIGINT NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id)
+);
 CREATE TABLE member (
                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
